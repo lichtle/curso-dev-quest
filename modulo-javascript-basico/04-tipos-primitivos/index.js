8d2b7b5b8b89e69fbf0959ccc180f2910c08f320
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-04 22:11:03 -0300

    Adiciona a pasta da aula 4

---
+++ modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,10 @@
+let nome = "Leticia";
+let numero = 72;
+let usuarioVerificado = false;
+let pessoa = null;
+
+/* if (usuarioVerificado) {
+  console.log("Usuário verificado");
+} else {
+  console.log("Usuário NÃO verificado");
+} */
