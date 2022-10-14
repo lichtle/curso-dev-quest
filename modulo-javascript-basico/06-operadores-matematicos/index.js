51c6befc26404eea10bfb55d212f284957928259
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-14 13:14:49 -0300

    Adiciona a pasta da aula 6

---
+++ modulo-javascript-basico/06-operadores-matematicos/index.js
@@ -0,0 +1,29 @@
+// SOMA (+)
+
+let primeiroValor = 4;
+let segundoValor = 2;
+
+console.log(primeiroValor + segundoValor); // O resultado será o número 6
+
+// Por conta da tipagem fraca do JS, é possível somar variáveis de tipos diferentes, por exemplo números com strings:
+
+let number = 5;
+let string = "6";
+
+console.log(number + string); // O resultado será a string "56"
+
+// SUBTRAÇÃO (-)
+
+console.log(primeiroValor - segundoValor); // O resultado será o número 2
+
+// DIVISÃO (/)
+
+console.log(primeiroValor / segundoValor); // O resultado será o número 2
+
+// MULTIPLICAÇÃO (*)
+
+console.log(primeiroValor * segundoValor); // O resultado será o número 8
+
+// RESTO DE UMA DIVISÃO (%)
+
+console.log(primeiroValor % segundoValor); // O resultado será o número 0
