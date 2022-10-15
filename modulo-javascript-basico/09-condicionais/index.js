c35c58e8db50c7e8fee6e515a0c9d890e4e0c737
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-15 17:13:23 -0300

    Adiciona a pasta da aula 9

---
+++ modulo-javascript-basico/09-condicionais/index.js
@@ -0,0 +1,15 @@
+let nomeFilme = "Batman";
+
+if (nomeFilme === "Coringa") {
+  console.log("É o filme do Coringa");
+} else if (nomeFilme === "Batman") {
+  console.log("É o filme do Batman");
+} else {
+  console.log("É outro filme");
+}
+
+/* OPERADOR TERNÁRIO (ideal para condicionais menores)
+
+nomeFilme === "Coringa"
+  ? console.log("É o filme do Coringa")
+  : console.log("É outro filme"); */
