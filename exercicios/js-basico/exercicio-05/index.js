014eea8fb04fc30fe73f67aff237a8ed642cc6fe
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-18 12:00:36 -0300

    Adiciona a pasta do exercício 5

---
+++ exercicios/js-basico/exercicio-05/index.js
@@ -0,0 +1,5 @@
+for (i = 0; i <= 10; i++) {
+  console.log("O dobro do número " + i + " é " + i * 2);
+}
+
+// Também é possível atribuir o valor i * 2 à uma variável: const dobro = i * 2. Sendo assim, console.log("O dobro do número " + i + " é " + dobro)
