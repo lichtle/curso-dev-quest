fc1d84f75c3a783eee8cc892a340822466721339
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-18 11:04:58 -0300

    Adiciona a pasta do exercício 3

---
+++ exercicios/js-basico/exercicio-03/index.js
@@ -0,0 +1,7 @@
+const passarNoite = confirm(
+  "Seja bem vinde, aventureiro! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?"
+);
+
+passarNoite
+  ? alert("Ótimo! Nós temos as melhores camas de toda a região!")
+  : alert("Que pena! Você parecia ser uma pessoa mais legal");
