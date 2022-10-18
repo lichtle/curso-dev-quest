9710cfc2a812ac1d41d9735892857d67d15cc4f4
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-18 10:47:49 -0300

    Alterações no exercício 2

--- exercicios/js-basico/exercicio-02/index.js
+++ exercicios/js-basico/exercicio-02/index.js
@@ -1,9 +1,9 @@
 const boletoPago = false;
 
 if (boletoPago) {
-  console.log("O boleto está pago");
-} else {
-  console.log("O boleto não está pago");
+  alert("O boleto está pago");
+} else if (boletoPago === false) {
+  alert("O boleto não está pago");
 }
 
 /* Outra resolução válida, usando operadore ternário:
@@ -11,7 +11,7 @@ if (boletoPago) {
 const boletoPago = false;
 
 boletoPago
-  ? console.log("O boleto está pago")
-  : console.log("O boleto não está pago");
+  ? alert("O boleto está pago")
+  : alert("O boleto não está pago");
 
 */
