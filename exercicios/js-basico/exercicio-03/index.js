194d9e4a14bca041eba55e35d9e3ad47716c9e81
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-18 11:38:51 -0300

    Alterações na resolução alternativa do exercício

--- exercicios/js-basico/exercicio-03/index.js
+++ exercicios/js-basico/exercicio-03/index.js
@@ -5,3 +5,17 @@ const vaiSeHospedar = confirm(
 vaiSeHospedar
   ? alert("Ótimo! Nós temos as melhores camas de toda a região!")
   : alert("Que pena! Você parecia ser uma pessoa mais legal");
+
+/* Usando if/else:
+
+const vaiSeHospedar = confirm(
+  "Seja bem vinde, aventureiro! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?"
+);
+
+if(vaiSeHospedar){
+  alert("Ótimo! Nós temos as melhores camas de toda a região!")
+} else if (vaiSeHospedar === false) {
+  alert("Que pena! Você parecia ser uma pessoa mais legal");
+}
+
+*/
