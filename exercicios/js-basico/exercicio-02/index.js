0933f035910bd6fb0279cfa2dbb56cd4b5b9ada6
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-18 10:36:54 -0300

    Alterações no exercício 2

--- exercicios/js-basico/exercicio-02/index.js
+++ exercicios/js-basico/exercicio-02/index.js
@@ -1,7 +1,17 @@
-let boletoPago = false;
+const boletoPago = false;
 
-if (boletoPago === true) {
+if (boletoPago) {
   console.log("O boleto está pago");
 } else {
   console.log("O boleto não está pago");
 }
+
+/* Outra resolução válida, usando operadore ternário:
+
+const boletoPago = false;
+
+boletoPago
+  ? console.log("O boleto está pago")
+  : console.log("O boleto não está pago");
+
+*/
