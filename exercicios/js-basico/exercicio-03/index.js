c64fc7bde472f1910908b213b5d2f7547f011107
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-18 11:28:22 -0300

    Muda o nome da variável para "vaiPassarNoite"

--- exercicios/js-basico/exercicio-03/index.js
+++ exercicios/js-basico/exercicio-03/index.js
@@ -1,7 +1,7 @@
-const passarNoite = confirm(
+const vaiPassarNoite = confirm(
   "Seja bem vinde, aventureiro! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?"
 );
 
-passarNoite
+vaiPassarNoite
   ? alert("Ótimo! Nós temos as melhores camas de toda a região!")
   : alert("Que pena! Você parecia ser uma pessoa mais legal");
