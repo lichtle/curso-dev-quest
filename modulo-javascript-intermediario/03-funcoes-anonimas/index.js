7e956291f694d53268b58b6ee040a0f723f1eb0c
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-19 13:03:35 -0300

    Muda a descrição da aula 2 e add pasta da aula 3

---
+++ modulo-javascript-intermediario/03-funcoes-anonimas/index.js
@@ -0,0 +1,18 @@
+/* ESTRUTURA NORMAL DE UMA FUNÇÃO:
+
+function somar(numero1, numero2) {
+  return numero1 + numero2;
+}
+
+let resultadoDaSoma = somar(12, 5);
+console.log(resultadoDaSoma);
+
+*/
+
+// ESTRUTURA DE UMA FUNÇÃO ANÔNIMA:
+
+let resultadoDaSoma = function (numero1, numero2) {
+  return numero1 + numero2;
+};
+
+console.log(resultadoDaSoma(12, 2));
