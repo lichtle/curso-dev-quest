2cfc9ed2581efe2ba4e08a92c116743f3ac34f0b
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-10-28 19:08:55 -0300

    Adiciona a pasta da aula 16

---
+++ modulo-javascript-intermediario/16-manipulando-html-dom/index.js
@@ -0,0 +1,27 @@
+function adicionarComentario() {
+  let inputComentario = document.getElementsByName("novo-comentario"); // 1) Acessa elementos através do valor do atributo name. Nesse caso estamos procurando um elemento que tenha o atributo name de valor "novo-comentario". A pesquisa resultará em uma NodeList com um único item, um input.
+
+  /* console.log(inputComentario);
+
+  >>> Output:
+  
+  NodeList [input]
+  0: input
+  length: 1
+  
+  */
+
+  let textoDigitado =
+    inputComentario[0]
+      .value; /* 2) Como a pesquisa anterior resultou em uma NodeList (array), precisamos indicar o índice do item cujo valor estamos buscando. Nesse caso o índice é 0, pois a pesquisa resultou em um array com apenas um item. Pegamos então o valor digitado nesse elemento, que é um input, através do método .value
+
+  console.log(textoDigitado);
+
+  >>> O output será o texto digitado no input.
+  
+  */
+
+  let novosComentarios = document.getElementById("novos-comentarios"); // 3) Pegando o elemento onde queremos colocar o comentário acima. Nesse caso, será uma div vazia de id "novos-comentarios"
+
+  novosComentarios.innerHTML += `<p>Seu comentário: ${textoDigitado}</p>`; // 4) Adicionando o comentário através do método .innerHTML, que insere porções de HTML dentro de determinado elemento. Nesse caso, o que queremos adicionar é um parágrafo contendo o comentário que foi escrito e armazenado na variável textoDigitado. Podemos utilizar o template literals (${...}) para concatenar textos e valores de variáveis sem a necessidade da utilização do sinal de +. Além disso, o += faz com que novas inserções de comentário apareçam uma abaixo da outra, pois do contrário cada comentário feito sobrescreveria o anterior. O += nesse exemplo é o mesmo que escrever novosComentarios.innerHTML = novosComentarios.innerHTML + `<p>Seu comentário: ${textoDigitado}</p>`
+}
