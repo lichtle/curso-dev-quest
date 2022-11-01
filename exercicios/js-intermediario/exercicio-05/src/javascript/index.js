e4fd3d29077cfceca571aeb3344a70d8f94294de
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-11-01 13:43:36 -0300

    Corrige o HTML e JS

--- exercicios/js-intermediario/exercicio-05/src/javascript/index.js
+++ exercicios/js-intermediario/exercicio-05/src/javascript/index.js
@@ -1,15 +1,56 @@
-const setaEsquerda = document.querySelectorAll(".seta-esquerda");
-const setaDireita = document.querySelectorAll(".seta-direita");
+const imagens = document.querySelectorAll(".slide");
+const setaVoltar = document.getElementById("seta-voltar");
+const setaAvancar = document.getElementById("seta-avancar");
 
-const primeiraImagem = document.getElementsByClassName("container-imagem-1");
-const segundaImagem = document.getElementsByClassName("container-imagem-2");
-const terceiraImagem = document.getElementsByClassName("container-imagem-3");
-const quartaImagem = document.getElementsByClassName("container-imagem-4");
+let imagemAtual = 0;
 
-let imagens = [primeiraImagem, segundaImagem, terceiraImagem, quartaImagem];
+setaAvancar.addEventListener("click", function () {
+  if (imagemAtual === imagens.length - 1) {
+    return;
+  } // Verifica se a imagem atual não é a última, porque se for esse eventListener não deve ocorrer. return é o comando para parar a execução da função caso a condição seja verdadeira. Essa condição deve ser verificada antes que o restante do código seja executado, por isso ela deve vir no topo da função
 
-setaDireita.forEach(function (setaAtual) {
-  setaAtual.addEventListener("click", function () {
-    alert("Olá!");
-  });
+  esconderImagemAberta(); // Esconde a imagem atual
+
+  imagemAtual++; // Vai para a próxima imagem
+
+  imagens[imagemAtual].classList.add("mostrar"); // Mostra a imagem atual
+
+  mostrarOuEsconderSetas(); // Mostra ou esconde as setas dependendo do índice da imagem atual
 });
+
+setaVoltar.addEventListener("click", function () {
+  if (imagemAtual === imagens[0]) {
+    return;
+  } // Verifica se a imagem atual não é a primeira, porque se for esse eventListener não deve ocorrer
+
+  esconderImagemAberta();
+
+  imagemAtual--;
+
+  imagens[imagemAtual].classList.add("mostrar");
+
+  mostrarOuEsconderSetas();
+});
+
+function esconderImagemAberta() {
+  const imagemAberta = document.querySelector(".mostrar");
+  imagemAberta.classList.remove("mostrar");
+} // Buscando o elemento de classe "mostrar" e removendo-a para esconder a imagem atual
+
+function mostrarOuEsconderSetas() {
+  const naoEhAPrimeiraImagem = imagemAtual !== 0;
+
+  if (naoEhAPrimeiraImagem) {
+    setaVoltar.classList.remove("opacidade");
+  } else {
+    setaVoltar.classList.add("opacidade");
+  } // Diz respeito à primeira imagem
+
+  const chegouNaUltimaImagem =
+    imagemAtual !== 0 && imagemAtual === imagens.length - 1;
+  if (chegouNaUltimaImagem) {
+    setaAvancar.classList.add("opacidade");
+  } else {
+    setaAvancar.classList.remove("opacidade");
+  } // Diz respeito à última imagem
+}
