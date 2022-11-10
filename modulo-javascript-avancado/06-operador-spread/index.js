487427accf5edd4c7d5b3b4cbab08042db1749d7
Leticia Negreiros <leticia.negreiros@outlook.com>
2022-11-10 20:58:47 -0300

    Adiciona a pasta da aula 6

---
+++ modulo-javascript-avancado/06-operador-spread/index.js
@@ -0,0 +1,35 @@
+const pessoas = ["Roberto", "Ricardo", "Raphael"];
+
+console.log(...pessoas); /* Retorna 3 itens separados. Dessa forma é possível, por exemplo, criar um novo array com esses itens + itens separados de outro array.
+
+>>> Antigamente era utilizado o .concat: 
+
+pessoas1 = grupo1.concat(grupo2).concat(grupo3) e assim por diante. 
+
+Agora considere os seguintes arrays: */
+
+let grupo1 = ["Roberto", "Ricardo", "Raphael"];
+let grupo2 = ["Lichtle", "Lizzy", "Jazmin", "Ruby"];
+
+grupo1 = [...grupo1, ...grupo2];
+
+console.log(grupo1);
+
+/* Output: (7) ['Roberto', 'Ricardo', 'Raphael', 'Lichtle', 'Lizzy', 'Jazmin', 'Ruby']
+
+>>> Além disso, o spread pode ser utilizado para clonar objetos:
+
+*/
+
+let personagem = {
+  nome: "Valquiria",
+  idade: 756,
+  altura: 1.56,
+  arma: "Adaga",
+};
+
+let cloneDoPersonagem = { ...personagem };
+
+console.log(personagem);
+console.log(cloneDoPersonagem);
+
