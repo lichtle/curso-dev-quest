5c3dc3ca0be42090b31d8988eb822ec2036c8411
Leticia Negreiros <leticia.negreiros@outlook.com>
2023-01-10 20:17:15 -0300

    Solução do exercício

--- exercicios/js-avancado/src/javascript/index.js
+++ exercicios/js-avancado/src/javascript/index.js
@@ -2,7 +2,7 @@ let idDoConselho = document.getElementById("advice-id");
 let conselho = document.querySelector(".advice");
 let botao = document.querySelector(".btn-dice");
 
-botao.addEventListener("click", async function gerarConselho() {
+async function gerarConselho() {
   try {
     const solicitacao = await fetch("https://api.adviceslip.com/advice"); // Irá retornar uma promise
     const dadosRecebidos = await solicitacao.json();
@@ -16,6 +16,8 @@ botao.addEventListener("click", async function gerarConselho() {
   } catch (err) {
     console.log(`O seguinte erro ocorreu: ${err}`);
   }
-});
+}
 
-/* idDoConselho.innerText = slip.id */
+gerarConselho();
+
+botao.addEventListener("click", gerarConselho);
