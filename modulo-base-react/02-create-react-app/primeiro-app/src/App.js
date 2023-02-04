ef59b92a1cf539b4756921556bb26bf474dbc333
Leticia Negreiros <leticia.negreiros@outlook.com>
2023-02-04 16:48:01 -0300

    Criação do meu primeiro componente React

--- modulo-base-react/02-create-react-app/primeiro-app/src/App.js
+++ modulo-base-react/02-create-react-app/primeiro-app/src/App.js
@@ -1,26 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/button/button.js";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world! I'm learning React. :D
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Button />
 }
 
 export default App;
-
