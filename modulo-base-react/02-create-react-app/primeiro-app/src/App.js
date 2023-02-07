6d892245ad27cd21918fb9c35e76fde2588d65a4
Leticia Negreiros <leticia.negreiros@outlook.com>
2023-02-07 18:52:26 -0300

    Props Children

--- modulo-base-react/02-create-react-app/primeiro-app/src/App.js
+++ modulo-base-react/02-create-react-app/primeiro-app/src/App.js
@@ -1,12 +1,20 @@
-import Button from "./components/button/button.js";
 import Card from "./components/card/card.js";
+import Button from "./components/button/button.js";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Card />
-      <Button />
+      <Card>
+        <h3>Título 1</h3>
+        <p>Este é o texto do card</p>
+      </Card>
+
+      <Card>
+        <h3>Título 2</h3>
+        <p>Este é o texto do card</p>
+        <Button />
+      </Card>
     </>
   );
 }
--- modulo-base-react/02-create-react-app/primeiro-app/src/components/card/card.js
+++ modulo-base-react/02-create-react-app/primeiro-app/src/components/card/card.js
@@ -1,15 +1,41 @@
 import React from "react";
-import './card.css'
+import "./card.css";
 
-class Card extends React.Component { // Este é um componente de CLASSE (e não de função). A palavra reservada "extends" significa que a classe Card está recebendo características do React. Pra que isso seja possível é necessário importar o React dentro do arquivo
-  render(){
+/* >>> Props em componentes de função:
+
+const Card = (props) => { 
+  return (
+    <div className="card">
+      <h3>{props.title}</h3>
+      <p>Este é o texto do card</p>
+    </div>
+  );
+}; 
+
+>>> OU utilizando shorthand notation, para extrair apenas as propriedades desejadas
+
+const Card = ({title}) => {
+  return (
+    <div className="card">
+      <h3>{title}</h3>
+      <p>Este é o texto do card</p>
+    </div>
+  );
+};
+
+>>> Props em componentes de classe:
+
+*/
+
+class Card extends React.Component {
+  // Este é um componente de CLASSE (e não de função). A palavra reservada "extends" significa que a classe Card está recebendo características do React. Pra que isso seja possível é necessário importar o React dentro do arquivo
+  render() {
     return (
       <div className="card">
-        <h3>Título</h3>
-        <p>Este é o texto do card</p>
+        {this.props.children}
       </div>
-    )
+    );
   }
 }
 
-export default Card
+export default Card;
