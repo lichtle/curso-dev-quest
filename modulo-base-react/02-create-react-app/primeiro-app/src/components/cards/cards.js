9ca864d63375730722c83f8c9b64fb1be92371ad
Leticia Negreiros <leticia.negreiros@outlook.com>
2023-02-08 19:35:31 -0300

    Listas no React

--- modulo-base-react/02-create-react-app/primeiro-app/src/components/cards/cards.js
+++ modulo-base-react/02-create-react-app/primeiro-app/src/components/cards/cards.js
@@ -1,24 +1,23 @@
 import Card from "../card/card.js";
 
+const cardsTitle = ["Título do card 1", "Título do card 2", "Título do card 3"];
+
 const Cards = () => {
   return (
     <div>
       <h2>Meus Cards</h2>
       <div>
-        <Card>
-          <h3 title="Título">Título do card 1</h3>
-          <p>Este é o texto do card 1</p>
-        </Card>
-
-        <Card>
-          <h3>Título do card 2</h3>
-          <p>Este é o texto do card 2</p>
-        </Card>
-
-        <Card>
-          <h3>Título do card 3</h3>
-          <p>Este é o texto do card 3</p>
-        </Card>
+        {cardsTitle.map((cardTitle, indice) => {
+          // O parâmetro "item" nesse caso é o cardTitle
+          return (
+            // Nesse caso nem é necessário usar o return
+            // Para trabalharmos com listas no React, é necessário atribuir uma chave única (prop key) para cada componente que está sendo criado (nesse caso o componente é o Card - a quantia irá depender de quantos elementos existem no array cardsTitle que estamos percorrendo para criar a lista). Para isso podemos usar o indice, que é o identificador único de posição de um elemento em um array:
+            <Card key={indice}>
+              <h3>{cardTitle}</h3>
+              <p>Esse é o texto do card 1</p>
+            </Card>
+          );
+        })}
       </div>
     </div>
   );
